d429d659952b4ed8d6468f10c956119bce766384
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-05-25 23:54:17 +0300

    add selectors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { connect } from "react-redux";
+import operations from "./redux/contacts/contacts-operations";
+import {
+  getLoading,
+  getErrorMessage,
+} from "./redux/contacts/contacts-selectors";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from "./components/Container";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import Filter from "./components/Filter";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchContacts();
+  }
+
+  render() {
+    const { isLoading, errorMessage } = this.props;
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+        {isLoading && <h2>Loading...</h2>}
+        {errorMessage && <h2>{errorMessage}</h2>}
+      </Container>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  isLoading: getLoading(state),
+  errorMessage: getErrorMessage(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchContacts: () => dispatch(operations.fetchContacts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
